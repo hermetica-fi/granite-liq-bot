17d4c353c17e102722080dd07732ca4fecb6fb59
talhasch <talhabugrabulut@gmail.com>
2025-01-28 20:39:38 +0300

    save

--- frontend/src/layout/index.tsx
+++ frontend/src/layout/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Paper, useTheme, Typography } from "@mui/material";
+import { Box, useTheme, Typography } from "@mui/material";
 import { grey } from "@mui/material/colors";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
@@ -17,25 +17,33 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
-
         color: theme.palette.mode === "light" ? grey[900] : grey[300],
         bgcolor: theme.palette.mode === "light" ? grey[50] : grey[900],
       }}
     >
-      <Paper
-      elevation={1}
+      <Box
         sx={{
           width: "100%",
           height: "50px",
           display: "flex",
           alignItems: "center",
-          pl: "12px",
-          pr: "12px",
-          mb: '20px'
+          mb: "20px",
+          boxShadow: `${theme.palette.divider} 1px 2px 10px 0px`,
         }}
       >
-        <Typography variant="h6">Granite Liqudiation Bot</Typography>
-      </Paper>
+        <Typography
+          variant="h6"
+          sx={{ ml: "12px", mr: "12px", display: "flex", alignItems: "center" }}
+        >
+          <Box
+            component="img"
+            sx={{ mr: "4px" }}
+            src="/icon.svg"
+            alt="Granite Liqudiation Bot"
+          />
+          Liquidation Bot
+        </Typography>
+      </Box>
       {children}
     </Box>
   );
