ac220b4342df3eb14ab3e8cbc159430e126d7310
talhasch <talhabugrabulut@gmail.com>
2025-01-28 20:20:34 +0300

    save

--- frontend/src/layout/index.tsx
+++ frontend/src/layout/index.tsx
@@ -1,4 +1,4 @@
-import { Box, useTheme } from "@mui/material";
+import { Box, Paper, useTheme, Typography } from "@mui/material";
 import { grey } from "@mui/material/colors";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
@@ -17,11 +17,25 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
-        justifyContent: "center",
+
         color: theme.palette.mode === "light" ? grey[900] : grey[300],
-        bgcolor: theme.palette.mode === "light" ? grey[100] : grey[900],
+        bgcolor: theme.palette.mode === "light" ? grey[50] : grey[900],
       }}
     >
+      <Paper
+      elevation={1}
+        sx={{
+          width: "100%",
+          height: "50px",
+          display: "flex",
+          alignItems: "center",
+          pl: "12px",
+          pr: "12px",
+          mb: '20px'
+        }}
+      >
+        <Typography variant="h6">Granite Liqudiation Bot</Typography>
+      </Paper>
       {children}
     </Box>
   );
