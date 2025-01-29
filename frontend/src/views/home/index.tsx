cc057262bcf0e4c1abc73e4e02ff8b232d0945a7
talhasch <talhabugrabulut@gmail.com>
2025-01-29 09:53:41 +0300

    save

--- frontend/src/views/home/index.tsx
+++ frontend/src/views/home/index.tsx
@@ -1,6 +1,8 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import AddContractForm from "../../components/add-contract-form";
 import { useContractsStore } from "../../store/contracts";
+import { grey } from "@mui/material/colors";
+import ThemedBox from "../../components/themed-box";
 
 const Home = () => {
   const { contracts } = useContractsStore();
@@ -13,7 +15,14 @@ const Home = () => {
     );
   }
 
-  return <div>Contracts</div>;
+  return (
+    <ThemedBox sx={{ maxWidth: "600px" }}>
+      <Typography variant="h6">Contracts</Typography>
+      <Typography fontSize="small" sx={{ mb: "20px", color: grey[600] }}>
+        List of liquidation contracts you have added.
+      </Typography>
+    </ThemedBox>
+  );
 };
 
 export default Home;
