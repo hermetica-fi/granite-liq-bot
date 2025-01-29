dbe09ff8c82253dd9de9268e568dda2835e84fb5
talhasch <talhabugrabulut@gmail.com>
2025-01-29 09:58:04 +0300

    save

--- frontend/src/views/home/index.tsx
+++ frontend/src/views/home/index.tsx
@@ -3,9 +3,11 @@ import AddContractForm from "../../components/add-contract-form";
 import { useContractsStore } from "../../store/contracts";
 import { grey } from "@mui/material/colors";
 import ThemedBox from "../../components/themed-box";
+import useTranslation from "../../hooks/use-translation";
 
 const Home = () => {
   const { contracts } = useContractsStore();
+  const [t] = useTranslation();
 
   if (contracts.length === 0) {
     return (
@@ -17,9 +19,9 @@ const Home = () => {
 
   return (
     <ThemedBox sx={{ maxWidth: "600px" }}>
-      <Typography variant="h6">Contracts</Typography>
+      <Typography variant="h6">{t('Contracts')}</Typography>
       <Typography fontSize="small" sx={{ mb: "20px", color: grey[600] }}>
-        List of liquidation contracts you have added.
+        {t('List of liquidation contracts you have added.')}
       </Typography>
     </ThemedBox>
   );
