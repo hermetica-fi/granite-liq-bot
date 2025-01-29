b3b73b6d5fb0131dbda3c6d39f3a8cba7321a0ce
talhasch <talhabugrabulut@gmail.com>
2025-01-29 10:43:57 +0300

    save

--- frontend/src/views/contract/index.tsx
+++ frontend/src/views/contract/index.tsx
@@ -1,7 +1,21 @@
-import { RouteComponentProps } from "@reach/router";
+import { RouteComponentProps, useParams } from "@reach/router";
+import { useContractsStore } from "../../store/contracts";
+import { Typography } from "@mui/material";
+import useTranslation from "../../hooks/use-translation";
 
 const ContractPage = (_: RouteComponentProps) => {
-    return <div>Contract</div>;
+  const { contracts } = useContractsStore();
+  const params = useParams();
+  const { id } = params;
+  const [t] = useTranslation();
+
+  const contract = contracts.find((c) => c.id === id);
+
+  if (!contract) {
+    return <Typography>{t("Not found")}</Typography>;
+  }
+
+  return <div>Contract</div>;
 };
 
 export default ContractPage;
