557325a85984ee0f4fbeda346703239c7bb800dd
talhasch <talhabugrabulut@gmail.com>
2025-01-29 20:52:59 +0300

    save

--- src/constants.ts
+++ src/constants.ts
@@ -2,9 +2,11 @@ export const CONTRACTS = {
     "mainnet": {
         "borrower": "SP35E2BBMDT2Y1HB0NTK139YBGYV3PAPK3WA8BRNA.borrower-v1",
         "state": "SP35E2BBMDT2Y1HB0NTK139YBGYV3PAPK3WA8BRNA.state-v1",
+        "ir": "SP35E2BBMDT2Y1HB0NTK139YBGYV3PAPK3WA8BRNA.linear-kinked-ir-v1"
     },
     "testnet": {
         "borrower": "ST20M5GABDT6WYJHXBT5CDH4501V1Q65242SPRMXH.borrower-v1",
         "state": "ST20M5GABDT6WYJHXBT5CDH4501V1Q65242SPRMXH.state-v1",
+        "ir": "ST20M5GABDT6WYJHXBT5CDH4501V1Q65242SPRMXH.linear-kinked-ir-v1"
     }
 }
