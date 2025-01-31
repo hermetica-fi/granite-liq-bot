5559a9e0c1e08f95107241b2246087a8db5d0303
talhasch <talhabugrabulut@gmail.com>
2025-01-31 18:04:50 +0300

    save

--- frontend/src/views/borrowers/index.tsx
+++ frontend/src/views/borrowers/index.tsx
@@ -1,6 +1,6 @@
-import { Box, Typography } from "@mui/material";
-import { RouteComponentProps } from "@reach/router";
-import { useEffect, useState } from "react";
+import { Box, MenuItem, Select, SelectChangeEvent, Typography } from "@mui/material";
+import { navigate, RouteComponentProps, useParams } from "@reach/router";
+import { useCallback, useEffect, useState } from "react";
 import { fetchBorrowers } from "../../api";
 import AppMenu from "../../components/app-menu";
 import BorrowersList from "../../components/borrowers-list";
@@ -10,20 +10,39 @@ import { Borrower } from "../../types";
 const BorrowersPage = (_: RouteComponentProps) => {
   const [t] = useTranslation();
   const [borrowers, setBorrowers] = useState<Borrower[]>([]);
+  const params = useParams();
+  const network = ["mainnet", "testnet"].includes(params.network)
+    ? params.network
+    : "mainnet";
 
   useEffect(() => {
-    fetchBorrowers("mainnet").then((data) => {
+    fetchBorrowers(network).then((data) => {
       setBorrowers(data);
     });
+  }, [network]);
+
+  const handleNetworkChange = useCallback((event: SelectChangeEvent) => {
+    navigate(`/borrowers/${event.target.value}`);
   }, []);
 
   return (
     <>
       <AppMenu />
       <Box sx={{ ml: "12px", mr: "12px" }}>
-        <Typography variant="h6" sx={{ mb: "20px" }}>
-          {t("Borrowers")}
-        </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            mb: "20px",
+          }}
+        >
+          <Typography variant="h6">{t("Borrowers")}</Typography>
+          <Select value={network} size="small" onChange={handleNetworkChange}>
+            <MenuItem value="mainnet">{t("Mainnet")}</MenuItem>
+            <MenuItem value="testnet">{t("Testnet")}</MenuItem>
+          </Select>
+        </Box>
 
         <BorrowersList borrowers={borrowers} />
       </Box>
