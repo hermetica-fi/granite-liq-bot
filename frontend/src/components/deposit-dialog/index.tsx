88bdfc6e658efb5fecb77f3e19a7f5686107790b
talhasch <talhabugrabulut@gmail.com>
2025-02-04 23:29:21 +0300

    save

--- frontend/src/components/deposit-dialog/index.tsx
+++ frontend/src/components/deposit-dialog/index.tsx
@@ -1,5 +1,5 @@
 import { LeatherProvider } from "@leather.io/rpc";
-import { Box } from "@mui/material";
+import { Box, Link, TextField, Typography } from "@mui/material";
 import Button from "@mui/material/Button";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
@@ -11,10 +11,15 @@ import {
   deserializeTransaction,
   makeUnsignedContractCall,
   Pc,
-  uintCV
+  uintCV,
 } from "@stacks/transactions";
-import { TESTNET_FEE } from "granite-liq-bot-common";
-import { useState } from "react";
+import {
+  parseUnits,
+  TESTNET_FEE,
+  transactionLink,
+} from "granite-liq-bot-common";
+import { useCallback, useMemo, useState } from "react";
+import useToast from "../../hooks/use-toast";
 import useTranslation from "../../hooks/use-translation";
 import { useModalStore } from "../../store/ui";
 import { Contract } from "../../types";
@@ -23,31 +28,48 @@ import CloseModal from "../close-modal";
 const DepositDialog = ({ contract }: { contract: Contract }) => {
   const [t] = useTranslation();
   const { setModal } = useModalStore();
-
+  const [showMessage] = useToast();
+  const [connection, setConnection] = useState<{
+    address: string;
+    publicKey: string;
+  } | null>(null);
+  const [txid, setTxid] = useState("");
+  const [amountRaw, setAmountRaw] = useState("");
   const [inProgress] = useState(false);
 
-  console.log(contract);
   const handleClose = () => {
     setModal(null);
   };
 
-  const handleSubmit = async () => {
-    const marketAsset = contract.marketAsset!;
-
-    const response = await window.LeatherProvider?.request("getAddresses");
-    if (!response) {
+  const handleSubmit = useCallback(async () => {
+    if (!window.LeatherProvider) {
+      showMessage("Leather Provider not found", "error");
       return;
     }
-    const { addresses } = response.result;
-    const stxAddress = addresses.find(
-      (address) => address.symbol === "STX"
-    )!.address;
-    const publicKey = addresses.find(
-      (address) => address.symbol === "STX"
-    )!.publicKey;
 
+    let address;
+    let publicKey;
 
+    if (!connection) {
+      const response = await window.LeatherProvider?.request("getAddresses");
+      if (!response) {
+        return;
+      }
 
+      const { addresses } = response.result;
+      address = addresses.find((address) => address.symbol === "STX")!.address;
+      publicKey = addresses.find(
+        (address) => address.symbol === "STX"
+      )!.publicKey;
+
+      setConnection({ address, publicKey });
+    } else {
+      address = connection.address;
+      publicKey = connection.publicKey;
+    }
+
+    const marketAsset = contract.marketAsset!;
+    const amount = parseUnits(amountRaw, marketAsset.decimals);
     const txOptions = {
       contractAddress: contract.address,
       contractName: contract.name,
@@ -57,14 +79,14 @@ const DepositDialog = ({ contract }: { contract: Contract }) => {
           marketAsset.address.split(".")[0],
           marketAsset.address.split(".")[1]
         ),
-        uintCV(100),
+        uintCV(amount),
       ],
       network: contract.network,
       publicKey,
       fee: contract.network === "testnet" ? TESTNET_FEE : undefined,
       postConditions: [
-        Pc.principal(stxAddress)
-          .willSendGte(100)
+        Pc.principal(address)
+          .willSendGte(amount)
           .ft(
             contract.marketAsset!.address as ContractIdString,
             contract.marketAsset!.name
@@ -73,24 +95,28 @@ const DepositDialog = ({ contract }: { contract: Contract }) => {
     };
 
     const transaction = await makeUnsignedContractCall(txOptions);
+    const sign = await (window.LeatherProvider as LeatherProvider)!.request(
+      "stx_signTransaction",
+      {
+        txHex: transaction.serialize(),
+        network: contract.network,
+      }
+    );
 
-    const response2 =
-      await (window.LeatherProvider as LeatherProvider)!.request(
-        "stx_signTransaction",
-        {
-          txHex: transaction.serialize(),
-          network: contract.network,
-        }
-      );
-
-    console.log(response2.result.txHex);
-
-    const response3 = await broadcastTransaction({
-      transaction: deserializeTransaction(response2.result.txHex),
+    const { txid } = await broadcastTransaction({
+      transaction: deserializeTransaction(sign.result.txHex),
       network: contract.network,
     });
 
-    console.log(response3.txid);
+    setTxid(txid);
+  }, [connection, contract, showMessage, amountRaw]);
+
+  const txLink = useMemo(() => {
+    return txid ? transactionLink(txid, contract.network) : "";
+  }, [txid, contract.network]);
+
+  const handleAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAmountRaw(e.target.value);
   };
 
   return (
@@ -100,7 +126,29 @@ const DepositDialog = ({ contract }: { contract: Contract }) => {
         <CloseModal onClick={handleClose} />
       </DialogTitle>
       <DialogContent>
-        <Box sx={{ p: "10px 0" }}></Box>
+        <Box sx={{ p: "10px 0" }}>
+          {txid ? (
+            <Box sx={{ wordBreak: "break-word" }}>
+              <Typography>
+                {t("A transaction successfully broadcasted to deposit:")}
+              </Typography>
+              <Typography sx={{ fontSize: "90%" }}>
+                <Link href={txLink} target="_blank" rel="noopener noreferrer">
+                  {txid}
+                </Link>
+              </Typography>
+            </Box>
+          ) : (
+            <TextField
+              fullWidth
+              label={t("Enter amount to deposit")}
+              value={amountRaw}
+              autoComplete="off"
+              helperText={t("e.g. 100")}
+              onChange={handleAmountChange}
+            />
+          )}
+        </Box>
       </DialogContent>
       <DialogActions>
         <Button onClick={handleSubmit} disabled={inProgress}>
