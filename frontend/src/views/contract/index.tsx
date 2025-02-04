717eb37c1d1753deab50dba9b6d6bc3159d2cbda
talhasch <talhabugrabulut@gmail.com>
2025-02-04 14:10:26 +0300

    contract info view

--- frontend/src/views/contract/index.tsx
+++ frontend/src/views/contract/index.tsx
@@ -1,13 +1,20 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableRow from "@mui/material/TableRow";
 import { RouteComponentProps, useParams } from "@reach/router";
 import { useEffect } from "react";
 import AppMenu from "../../components/app-menu";
 import useTranslation from "../../hooks/use-translation";
 import { useContractStore } from "../../store/contract";
 import { useContractsStore } from "../../store/contracts";
+
 const ContractPage = (_: RouteComponentProps) => {
   const { contracts } = useContractsStore();
-  const {  loadContract, loading } = useContractStore();
+  const { loadContract, data, loading } = useContractStore();
   const params = useParams();
   const { id } = params;
   const [t] = useTranslation();
@@ -24,13 +31,115 @@ const ContractPage = (_: RouteComponentProps) => {
     return <Typography>{t("Not found")}</Typography>;
   }
 
-  console.log(loading)
+  if (loading) {
+    // return <CircularProgress />;
+  }
+
+  if (!data) {
+    return null;
+  }
 
   return (
     <>
       <AppMenu network={contract.network} />
       <Box sx={{ ml: "12px", mr: "12px" }}>
-        <Typography variant="h6">{id}</Typography>
+        <Typography variant="h6" sx={{ mb: "20px" }}>
+          {id}
+        </Typography>
+        <TableContainer component={Paper}>
+          <Table>
+            <TableBody>
+              <TableRow>
+                <TableCell component="th" scope="row" sx={{ width: "200px" }}>
+                  <Typography sx={{ fontWeight: "500" }}>
+                    {t("Owner")}
+                  </Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography>{data.ownerAddress}</Typography>
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell
+                  component="th"
+                  scope="row"
+                  sx={{ fontWeight: "bold" }}
+                >
+                  <Typography sx={{ fontWeight: "500" }}>
+                    {t("Operator")}
+                  </Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography>{data.operatorAddress}</Typography>
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell
+                  component="th"
+                  scope="row"
+                  sx={{ fontWeight: "bold" }}
+                >
+                  <Typography sx={{ fontWeight: "500" }}>
+                    {t("Operator balance")}
+                  </Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography>{data.operatorBalance}</Typography>
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell
+                  component="th"
+                  scope="row"
+                  sx={{ fontWeight: "bold" }}
+                >
+                  <Typography sx={{ fontWeight: "500" }}>
+                    {t("Market assets")}
+                  </Typography>
+                </TableCell>
+                <TableCell sx={{ display: "flex", alignItems: "center" }}>
+                  <Box sx={{ mr: "12px" }}>
+                    {data.marketAssets.length > 0 ? (
+                      <Typography>{data.marketAssets.join(", ")}</Typography>
+                    ) : (
+                      <Typography>{t("-")}</Typography>
+                    )}
+                  </Box>
+                  <Button variant="outlined">Add</Button>
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell
+                  component="th"
+                  scope="row"
+                  sx={{ fontWeight: "bold" }}
+                >
+                  <Typography sx={{ fontWeight: "500" }}>
+                    {t("Unprofitability threshold")}
+                  </Typography>
+                </TableCell>
+                <TableCell sx={{ display: "flex", alignItems: "center" }}>
+                  <Typography sx={{ mr: "12px" }}>
+                    {data.unprofitabilityThreshold}
+                  </Typography>
+                  <Button size="small" variant="outlined">Set</Button>
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell
+                  component="th"
+                  scope="row"
+                  sx={{ fontWeight: "bold" }}
+                >
+                  <Typography sx={{ fontWeight: "500" }}>
+                    {t("Balances")}
+                  </Typography>
+                </TableCell>
+                <TableCell></TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
       </Box>
     </>
   );
