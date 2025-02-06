45af8ffa3e7c9824175398b2fe7f1766e271bc2e
talhasch <talhabugrabulut@gmail.com>
2025-02-06 11:26:10 +0300

    no need pyth contract

--- src/constants.ts
+++ src/constants.ts
@@ -11,14 +11,12 @@ export const CONTRACTS = {
     "mainnet": {
         "borrower": "SP35E2BBMDT2Y1HB0NTK139YBGYV3PAPK3WA8BRNA.borrower-v1",
         "state": "SP35E2BBMDT2Y1HB0NTK139YBGYV3PAPK3WA8BRNA.state-v1",
-        "ir": "SP35E2BBMDT2Y1HB0NTK139YBGYV3PAPK3WA8BRNA.linear-kinked-ir-v1",
-        "pyth-adapter": "SP35E2BBMDT2Y1HB0NTK139YBGYV3PAPK3WA8BRNA.pyth-adapter-v1"
+        "ir": "SP35E2BBMDT2Y1HB0NTK139YBGYV3PAPK3WA8BRNA.linear-kinked-ir-v1"
     },
     "testnet": {
         "borrower": "ST20M5GABDT6WYJHXBT5CDH4501V1Q65242SPRMXH.borrower-v1",
         "state": "ST20M5GABDT6WYJHXBT5CDH4501V1Q65242SPRMXH.state-v1",
-        "ir": "ST20M5GABDT6WYJHXBT5CDH4501V1Q65242SPRMXH.linear-kinked-ir-v1",
-        "pyth-adapter": "ST20M5GABDT6WYJHXBT5CDH4501V1Q65242SPRMXH.pyth-adapter-v1"
+        "ir": "ST20M5GABDT6WYJHXBT5CDH4501V1Q65242SPRMXH.linear-kinked-ir-v1"
     }
 }
 
