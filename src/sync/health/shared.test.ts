668aab89daa2c37f062885eab30a0efcb40fcdfd
talhasch <talhabugrabulut@gmail.com>
2025-02-07 12:55:57 +0300

    test fix for new price feed logic

--- src/sync/health/shared.test.ts
+++ src/sync/health/shared.test.ts
@@ -1,72 +1,78 @@
 import { expect, setSystemTime, test } from "bun:test";
+import type { PriceFeedResponse } from "../../client/pyth";
 import type { MarketState } from "../../types";
 import { calcBorrowerStatus } from "./shared";
 
-const priceFeed = {
-    "attestation": "0",
-    "items": {
-      "btc": {
-        "id": "e62df6c8b4a85fe1a67db44dc12de5db330f7ac66b72dc658afedf0f4a415b43",
-        "price": {
-          "price": "9641980806249",
-          "conf": "4069193751",
-          "expo": -8,
-          "publish_time": 1738867653
-        },
-        "ema_price": {
-          "price": "9661765600000",
-          "conf": "4777017400",
-          "expo": -8,
-          "publish_time": 1738867653
-        },
-        "metadata": {
-          "slot": 196328588,
-          "proof_available_time": 1738867654,
-          "prev_publish_time": 1738867652
+
+
+const makePriceFeed = (btc: string, eth: string, usdc: string): PriceFeedResponse => {
+    return {
+        "attestation": "0",
+        "items": {
+            "btc": {
+                "id": "e62df6c8b4a85fe1a67db44dc12de5db330f7ac66b72dc658afedf0f4a415b43",
+                "price": {
+                    "price": btc,
+                    "conf": "4069193751",
+                    "expo": -8,
+                    "publish_time": 1738867653
+                },
+                "ema_price": {
+                    "price": btc,
+                    "conf": "4777017400",
+                    "expo": -8,
+                    "publish_time": 1738867653
+                },
+                "metadata": {
+                    "slot": 196328588,
+                    "proof_available_time": 1738867654,
+                    "prev_publish_time": 1738867652
+                }
+            },
+            "eth": {
+                "id": "ff61491a931112ddf1bd8147cd1b641375f79f5825126d665480874634fd0ace",
+                "price": {
+                    "price": eth,
+                    "conf": "131951019",
+                    "expo": -8,
+                    "publish_time": 1738867653
+                },
+                "ema_price": {
+                    "price": eth,
+                    "conf": "160691626",
+                    "expo": -8,
+                    "publish_time": 1738867653
+                },
+                "metadata": {
+                    "slot": 196328588,
+                    "proof_available_time": 1738867654,
+                    "prev_publish_time": 1738867652
+                }
+            },
+            "usdc": {
+                "id": "eaa020c61cc479712813461ce153894a96a6c00b21ed0cfc2798d1f9a9e9c94a",
+                "price": {
+                    "price": usdc,
+                    "conf": "104164",
+                    "expo": -8,
+                    "publish_time": 1738867653
+                },
+                "ema_price": {
+                    "price": usdc,
+                    "conf": "112524",
+                    "expo": -8,
+                    "publish_time": 1738867653
+                },
+                "metadata": {
+                    "slot": 196328588,
+                    "proof_available_time": 1738867654,
+                    "prev_publish_time": 1738867652
+                }
+            }
         }
-      },
-      "eth": {
-        "id": "ff61491a931112ddf1bd8147cd1b641375f79f5825126d665480874634fd0ace",
-        "price": {
-          "price": "269889923204",
-          "conf": "131951019",
-          "expo": -8,
-          "publish_time": 1738867653
-        },
-        "ema_price": {
-          "price": "270519220000",
-          "conf": "160691626",
-          "expo": -8,
-          "publish_time": 1738867653
-        },
-        "metadata": {
-          "slot": 196328588,
-          "proof_available_time": 1738867654,
-          "prev_publish_time": 1738867652
-        }
-      },
-      "usdc": {
-        "id": "eaa020c61cc479712813461ce153894a96a6c00b21ed0cfc2798d1f9a9e9c94a",
-        "price": {
-          "price": "99997731",
-          "conf": "104164",
-          "expo": -8,
-          "publish_time": 1738867653
-        },
-        "ema_price": {
-          "price": "99997983",
-          "conf": "112524",
-          "expo": -8,
-          "publish_time": 1738867653
-        },
-        "metadata": {
-          "slot": 196328588,
-          "proof_available_time": 1738867654,
-          "prev_publish_time": 1738867652
-        }
-      }
     }
-  }
+}
+
 
 test("calcBorrowerStatus 1", () => {
     setSystemTime(1738262052565);
@@ -98,13 +104,6 @@ test("calcBorrowerStatus 1", () => {
                 maxLTV: 50000000,
             },
         },
-        /*
-        priceFeed: {
-            btc: 10524868578626,
-            eth: 326603000000,
-            usdc: 100005237,
-        },
-        */
         marketAssetParams: {
             decimals: 8
         }
@@ -118,7 +117,7 @@ test("calcBorrowerStatus 1", () => {
         }
     }
 
-    expect(calcBorrowerStatus(borrower, marketState, priceFeed)).toEqual({
+    expect(calcBorrowerStatus(borrower, marketState, makePriceFeed("10524868578626", "326603000000", "100005237"))).toEqual({
         ltv: 0.6977874992015272,
         health: 1.0206104956758972,
         debt: 526735.7296664099,
@@ -159,13 +158,6 @@ test("calcBorrowerStatus 2", () => {
                 maxLTV: 50000000,
             },
         },
-        /*
-        priceFeed: {
-            btc: 10549298752013,
-            eth: 327439303121,
-            usdc: 100003543,
-        },
-        */
         marketAssetParams: {
             decimals: 8
         }
@@ -178,7 +170,7 @@ test("calcBorrowerStatus 2", () => {
         }
     }
 
-    expect(calcBorrowerStatus(borrower, marketState, priceFeed)).toEqual({
+    expect(calcBorrowerStatus(borrower, marketState, makePriceFeed("10549298752013", "327439303121", "100003543"))).toEqual({
         ltv: 0.6976290663298624,
         health: 0.7310475216909251,
         debt: 15038.024870804698,
