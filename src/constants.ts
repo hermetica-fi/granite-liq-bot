4717931e5191e19782c7a6cd51dd4845ec4839ee
talhasch <talhabugrabulut@gmail.com>
2025-02-11 14:25:20 +0300

    save

--- src/constants.ts
+++ src/constants.ts
@@ -12,7 +12,7 @@ export const CONTRACTS = {
         "borrower": "SP35E2BBMDT2Y1HB0NTK139YBGYV3PAPK3WA8BRNA.borrower-v1",
         "state": "SP35E2BBMDT2Y1HB0NTK139YBGYV3PAPK3WA8BRNA.state-v1",
         "ir": "SP35E2BBMDT2Y1HB0NTK139YBGYV3PAPK3WA8BRNA.linear-kinked-ir-v1",
-        "liquidator": "SP3R4F6C1J3JQWWCVZ3S7FRRYPMYG6ZW6RZK31FXY.liquidator-v1"
+        "liquidator": "SP35E2BBMDT2Y1HB0NTK139YBGYV3PAPK3WA8BRNA.liquidator-v1"
     },
     "testnet": {
         "borrower": "ST20M5GABDT6WYJHXBT5CDH4501V1Q65242SPRMXH.borrower-v1",
