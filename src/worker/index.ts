504c7b85064a5fce6ccecbaac9b33420628c839f
talhasch <talhabugrabulut@gmail.com>
2025-02-17 14:43:05 +0300

    print contracts on worker boot

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,14 +1,18 @@
 import { sleep } from "bun";
+import { CONTRACTS } from "../constants";
 import { main as borrowerSync } from "./borrower-sync";
 import { main as contractSync } from "./contract-sync";
 import { main as eventSync } from "./event-sync";
 import { main as healthSync } from "./health-sync";
-import { main as marketSync } from "./market-sync";
 import { main as liquidate } from "./liquidate";
-
+import { main as marketSync } from "./market-sync";
 const BASE_DELAY = 7_000;
 
 export const main = async () => {
+    console.log("--------------------------------");
+    console.log("Worker started with contracts:")
+    console.log(CONTRACTS); 
+    console.log("--------------------------------");
     while (true) {
         const start = Date.now();
         await eventSync();
