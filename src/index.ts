755c2a91d85b925398b762e29d53d1e035b890e9
talhasch <talhabugrabulut@gmail.com>
2025-02-25 13:33:14 +0300

    merge web and worker

--- src/index.ts
+++ src/index.ts
@@ -3,37 +3,18 @@ import { waitForDb } from './db';
 import { migrateDb } from './db/migrate';
 import { createLogger } from './logger';
 import { main as workerMain } from './worker';
+
 const logger = createLogger('main');
 
-const main = async () => {
-    const cmd = process.argv[process.argv.length - 1];
-
-    let prms: () => Promise<void>;
-
-    if (cmd === "api") {
-        prms = apiMain;
-    } else if (cmd === "worker") {
-        prms = workerMain;
-    } else {
-        throw new Error("Invalid command");
-    }
-
-    await waitForDb(60, 2000).catch((e) => {
-        logger.error(`Couldn't connect to database`);
-        throw e;
-    });
-
-    await migrateDb().catch(e => {
-        logger.error(`Couldn't migrate database due to: ${e}`);
-        throw e;
-    });
-
-    await prms();
-}
-
-main().catch((e) => {
-    logger.error(e.toString());
-    process.exit(1);
+await waitForDb(60, 2000).catch((e) => {
+    logger.error(`Couldn't connect to database`);
+    throw e;
 });
 
+await migrateDb().catch(e => {
+    logger.error(`Couldn't migrate database due to: ${e}`);
+    throw e;
+});
 
+apiMain();
+workerMain();
--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,30 +1,43 @@
-import { sleep } from "bun";
 import { CONTRACTS } from "../constants";
+import { createLogger } from "../logger";
 import { main as borrowerSync } from "./borrower-sync";
 import { main as contractSync } from "./contract-sync";
 import { main as eventSync } from "./event-sync";
 import { main as healthSync } from "./health-sync";
 import { main as liquidate } from "./liquidate";
 import { main as marketSync } from "./market-sync";
+
 const BASE_DELAY = 7_000;
+
+const logger = createLogger('worker');
+
+const workerInner = async () => {
+    await eventSync();
+    await borrowerSync();
+    await marketSync();
+    await healthSync();
+    await contractSync();
+    await liquidate();
+}
+
+const worker = async () => {
+    const start = Date.now();
+
+    await workerInner().catch((e) => {
+        logger.error(e.toString());
+        process.exit(1);
+    });
+
+    const end = Date.now();
+    const delay = Math.max(1000, BASE_DELAY - (end - start));
+
+    setTimeout(worker, delay);
+}
 
 export const main = async () => {
     console.log("--------------------------------");
     console.log("Worker started with contracts:")
     console.log(CONTRACTS); 
     console.log("--------------------------------");
-    while (true) {
-        const start = Date.now();
-        await eventSync();
-        await borrowerSync();
-        await marketSync();
-        await healthSync();
-        await contractSync();
-        await liquidate();
-        const end = Date.now();
-        const delay = BASE_DELAY - (end - start);
-        if (delay > 0) {
-            await sleep(delay)
-        }
-    }
+    worker();
 }
