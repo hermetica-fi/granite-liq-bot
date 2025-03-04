b762204a96d68928cb29e79b57ff18acb14f331a
talhasch <talhabugrabulut@gmail.com>
2025-03-04 14:13:36 +0300

    varchar to text

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -4,61 +4,61 @@ import { pool } from "../db";
 
 export const createDb = async (client: PoolClient) => {
     let CREATE = "CREATE TABLE IF NOT EXISTS public.kv_store(" +
-        "key VARCHAR PRIMARY KEY NOT NULL," +
-        "value VARCHAR NOT NULL," +
+        "key TEXT PRIMARY KEY NOT NULL," +
+        "value TEXT NOT NULL," +
         "expires INTEGER NOT NULL DEFAULT 0" +
         ");";
 
     CREATE += "CREATE TABLE IF NOT EXISTS public.contract(" +
-        "id VARCHAR PRIMARY KEY NOT NULL," +
-        "address VARCHAR NOT NULL," +
-        "name VARCHAR NOT NULL," +
-        "network VARCHAR NOT NULL UNIQUE," +
-        "operator_address VARCHAR NOT NULL," +
-        "operator_priv VARCHAR NOT NULL," +
-        "market_asset VARCHAR," +
+        "id TEXT PRIMARY KEY NOT NULL," +
+        "address TEXT NOT NULL," +
+        "name TEXT NOT NULL," +
+        "network TEXT NOT NULL UNIQUE," +
+        "operator_address TEXT NOT NULL," +
+        "operator_priv TEXT NOT NULL," +
+        "market_asset TEXT," +
         "market_asset_balance REAL NOT NULL DEFAULT 0," +
-        "collateral_asset VARCHAR," +
+        "collateral_asset TEXT," +
         "collateral_asset_balance REAL NOT NULL DEFAULT 0," +
-        "lock_tx VARCHAR," +
+        "lock_tx TEXT," +
         "unlocks_at INTEGER," +
         "created_at INTEGER NOT NULL" +
         ");";
 
     CREATE += "CREATE TABLE IF NOT EXISTS public.borrower(" +
-        "address VARCHAR PRIMARY KEY NOT NULL," +
-        "network VARCHAR NOT NULL," +
+        "address TEXT PRIMARY KEY NOT NULL," +
+        "network TEXT NOT NULL," +
         "sync_flag INTEGER NOT NULL DEFAULT 1," +
         "sync_ts INTEGER NOT NULL DEFAULT 0" +
         ");";
 
 
     CREATE += "CREATE TABLE IF NOT EXISTS public.borrower_position(" +
-        "address VARCHAR PRIMARY KEY REFERENCES borrower(address) ON DELETE RESTRICT," +
-        "network VARCHAR NOT NULL," +
+        "address TEXT PRIMARY KEY REFERENCES borrower(address) ON DELETE RESTRICT," +
+        "network TEXT NOT NULL," +
         "debt_shares REAL NOT NULL," +
-        "collaterals VARCHAR NOT NULL" +
+        "collaterals TEXT NOT NULL" +
         ");";
 
 
     CREATE += "CREATE TABLE IF NOT EXISTS public.borrower_collaterals(" +
-        "address VARCHAR NOT NULL REFERENCES borrower(address) ON DELETE RESTRICT," +
-        "network VARCHAR NOT NULL," +
-        "collateral VARCHAR NOT NULL," +
+        "address TEXT NOT NULL REFERENCES borrower(address) ON DELETE RESTRICT," +
+        "network TEXT NOT NULL," +
+        "collateral TEXT NOT NULL," +
         "amount REAL NOT NULL" +
         ");";
 
     CREATE += "CREATE UNIQUE INDEX IF NOT EXISTS borrower_collateral_address_idx ON borrower_collaterals (address, collateral);";
 
     CREATE += "CREATE TABLE IF NOT EXISTS public.borrower_status(" +
-        "address VARCHAR PRIMARY KEY REFERENCES borrower(address) ON DELETE RESTRICT," +
-        "network VARCHAR NOT NULL," +
+        "address TEXT PRIMARY KEY REFERENCES borrower(address) ON DELETE RESTRICT," +
+        "network TEXT NOT NULL," +
         "ltv REAL NOT NULL," +
         "health REAL NOT NULL," +
         "debt REAL NOT NULL," +
         "collateral REAL NOT NULL," +
         "risk REAL NOT NULL," +
-        "max_repay VARCHAR NOT NULL," +
+        "max_repay TEXT NOT NULL," +
         "total_repay_amount REAL NOT NULL" +
         ");";
 
