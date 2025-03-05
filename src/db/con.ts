06d32e603aad49c26b598925b42f09245fe35fc0
talhasch <talhabugrabulut@gmail.com>
2025-03-05 17:21:22 +0300

    create data directory if not exists

--- src/db/con.ts
+++ src/db/con.ts
@@ -1,5 +1,10 @@
 import { Database } from "bun:sqlite";
+import fs from "fs";
 
 const IS_TEST = process.env.NODE_ENV === 'test';
+
+if(!fs.existsSync("./data")) {
+    fs.mkdirSync("./data", { recursive: true });
+}
 
 export const dbCon = new Database(IS_TEST ? ":memory:" : "./data/db.sqlite", { strict: true });
