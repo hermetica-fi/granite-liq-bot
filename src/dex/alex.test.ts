b640e73c1c67a0ceb819476534ca2a3fa6deffe4
Talha Bugra Bulut <talhabugrabulut@gmail.com>
2025-04-17 15:18:18 +0300

    simplfy alex estimate fn

--- src/dex/alex.test.ts
+++ src/dex/alex.test.ts
@@ -1,10 +1,8 @@
-import { cvToJSON } from "@stacks/transactions";
 import { describe, expect, mock, test } from "bun:test";
-import { getBestSwap } from "./alex";
+import { estimateSbtcToAeusdc } from "./alex";
 
 describe("alex", () => {
-    test("getBestSwap", async () => {
-
+    test("estimateSbtcToAeusdc", async () => {
         mock.module("../client/stxer", () => {
             return {
                 batchContractRead: () => {
@@ -17,174 +15,56 @@ describe("alex", () => {
             }
         });
 
-        const result = await getBestSwap(0.01);
-
-        const pathJS = result?.option.path.map(x => cvToJSON(x));
-        expect(pathJS).toEqual([
-            {
-              type: "principal",
-              value: "SP1E0XBN9T4B10E9QMR7XMFJPMA19D77WY3KP2QKC.token-wsbtc",
-            }, {
-              type: "principal",
-              value: "SP102V8P0F7JX67ARQ77WEA3D3CFB5XW39REDT0AM.token-wstx-v2",
-            }, {
-              type: "principal",
-              value: "SP102V8P0F7JX67ARQ77WEA3D3CFB5XW39REDT0AM.token-waeusdc",
-            }
-          ]);
-
-        const factorsJS = result?.option.factors.map(x => cvToJSON(x));
-        expect(factorsJS).toEqual([
-            {
-              type: "uint",
-              value: "100000000",
-            }, {
-              type: "uint",
-              value: "100000000",
-            }
-          ],);
-
-        expect(result?.out).toEqual(938.15654752);
+        const result = await estimateSbtcToAeusdc(0.01);
+        expect(result).toEqual(938.15654752);
     });
+
+    test("estimateSbtcToAeusdc err 1", async () => {
+        mock.module("../client/stxer", () => {
+            return {
+                batchContractRead: () => {
+                    return [
+                        {
+                            Ok: "0801000000000000000000000000000007db",
+                        },
+                    ]
+                }
+            }
+        });
+
+        const result = await estimateSbtcToAeusdc(0.01);
+        expect(result).toEqual(0);
+    });
+
+    test("estimateSbtcToAeusdc err 2", async () => {
+        mock.module("../client/stxer", () => {
+            return {
+                batchContractRead: () => {
+                    return [
+                        {
+                            Err: "0701000000000000000000000015d7d94960",
+                        },
+                    ]
+                }
+            }
+        });
+
+        const result = await estimateSbtcToAeusdc(0.01);
+        expect(result).toEqual(0);
+    });
+
+    test("estimateSbtcToAeusdc err 3", async () => {
+        mock.module("../client/stxer", () => {
+            return {
+                batchContractRead: () => {
+                    return [
+                        
+                    ]
+                }
+            }
+        });
+
+        const result = await estimateSbtcToAeusdc(0.01);
+        expect(result).toEqual(0);
+    });    
 });
-
-/* tests for disabled routes
-describe("alex", () => {
-    test("getBestSwap", async () => {
-
-        mock.module("./client/stxer", () => {
-            return {
-                batchContractRead: () => {
-                    return [
-                        {
-                            Ok: "07010000000000000000000000170ce2f6ef",
-                        }, {
-                            Ok: "07010000000000000000000000177802a131",
-                        }, {
-                            Ok: "07010000000000000000000000171bd413c8",
-                        }, {
-                            Ok: "0701000000000000000000000017372e7ede",
-                        }
-                    ]
-                }
-            }
-        });
-
-        const result = await getBestSwap(0.01);
-
-        const pathJS = result?.option.path.map(x => cvToJSON(x));
-        expect(pathJS).toEqual([
-            {
-                type: "principal",
-                value: "SP1E0XBN9T4B10E9QMR7XMFJPMA19D77WY3KP2QKC.token-wsbtc",
-            }, {
-                type: "principal",
-                value: "SP102V8P0F7JX67ARQ77WEA3D3CFB5XW39REDT0AM.token-alex",
-            }, {
-                type: "principal",
-                value: "SP102V8P0F7JX67ARQ77WEA3D3CFB5XW39REDT0AM.token-wstx-v2",
-            }, {
-                type: "principal",
-                value: "SP102V8P0F7JX67ARQ77WEA3D3CFB5XW39REDT0AM.token-waeusdc",
-            }
-        ]);
-
-        const factorsJS = result?.option.factors.map(x => cvToJSON(x));
-        expect(factorsJS).toEqual([
-            {
-                type: "uint",
-                value: "100000000",
-            }, {
-                type: "uint",
-                value: "100000000",
-            }, {
-                type: "uint",
-                value: "100000000",
-            }
-        ]);
-
-        expect(result?.out).toEqual(1007.97686065);
-    });
-
-
-    test("getBestSwap", async () => {
-
-        mock.module("./client/stxer", () => {
-            return {
-                batchContractRead: () => {
-                    return [
-                        {
-                            Ok: "0801000000000000000000000000000007dc", // err 2012n
-                        }, {
-                            Ok: "070100000000000000000000007057dc470a",
-                        }, {
-                            Ok: "070100000000000000000000006f27be6bcc",
-                        }, {
-                            Ok: "0701000000000000000000000071af841920",
-                        }
-                    ]
-                }
-            }
-        });
-
-        const result = await getBestSwap(0.05);
-
-        const pathJS = result?.option.path.map(x => cvToJSON(x));
-        expect(pathJS).toEqual([
-            {
-                type: "principal",
-                value: "SP1E0XBN9T4B10E9QMR7XMFJPMA19D77WY3KP2QKC.token-wsbtc",
-            }, {
-                type: "principal",
-                value: "SP2XD7417HGPRTREMKF748VNEQPDRR0RMANB7X1NK.token-abtc",
-            }, {
-                type: "principal",
-                value: "SP102V8P0F7JX67ARQ77WEA3D3CFB5XW39REDT0AM.token-wstx-v2",
-            }, {
-                type: "principal",
-                value: "SP102V8P0F7JX67ARQ77WEA3D3CFB5XW39REDT0AM.token-waeusdc",
-            }
-        ]);
-
-        const factorsJS = result?.option.factors.map(x => cvToJSON(x));
-        expect(factorsJS).toEqual([
-            {
-                type: "uint",
-                value: "5000000",
-            }, {
-                type: "uint",
-                value: "100000000",
-            }, {
-                type: "uint",
-                value: "100000000",
-            }
-        ]);
-
-        expect(result?.out).toEqual(4882.75974432);
-    });
-
-
-    test("getBestSwap", async () => {
-        mock.module("./client/stxer", () => {
-            return {
-                batchContractRead: () => {
-                    return [
-                        {
-                            Ok: "0801000000000000000000000000000007dc",  // err 2012n
-                        }, {
-                            Ok: "0801000000000000000000000000000007dc", // err 2012n
-                        }, {
-                            Ok: "0801000000000000000000000000000007dc", // err 2012n
-                        }, {
-                            Ok: "0801000000000000000000000000000007dc", // err 2012n
-                        }
-                    ]
-                }
-            }
-        });
-
-        const result = await getBestSwap(0.3);
-
-        expect(result?.out).toEqual(0);
-    });
-});*/
--- src/dex/alex.ts
+++ src/dex/alex.ts
@@ -1,92 +1,26 @@
-import { contractPrincipalCV, cvToJSON, deserializeCV, serializeCV, uintCV, type ClarityValue } from "@stacks/transactions";
+import { contractPrincipalCV, cvToJSON, deserializeCV, serializeCV, uintCV } from "@stacks/transactions";
 import { batchContractRead, type ReadCall } from "../client/stxer";
 import { formatUnits, parseUnits } from "../units";
 
-const factor05 = uintCV(5000000);
-const factor1 = uintCV(100000000);
-const router = contractPrincipalCV('SP102V8P0F7JX67ARQ77WEA3D3CFB5XW39REDT0AM', 'amm-pool-v2-01');
+export const estimateSbtcToAeusdc = async (sBtcAmount: number) => {
+    const call: ReadCall = [
+        serializeCV(contractPrincipalCV('SP102V8P0F7JX67ARQ77WEA3D3CFB5XW39REDT0AM', 'amm-pool-v2-01')),
+        'get-helper-a',
+        serializeCV(contractPrincipalCV("SP1E0XBN9T4B10E9QMR7XMFJPMA19D77WY3KP2QKC", "token-wsbtc")),
+        serializeCV(contractPrincipalCV("SP102V8P0F7JX67ARQ77WEA3D3CFB5XW39REDT0AM", "token-wstx-v2")),
+        serializeCV(contractPrincipalCV("SP102V8P0F7JX67ARQ77WEA3D3CFB5XW39REDT0AM", "token-waeusdc")),
+        serializeCV(uintCV(100000000)),
+        serializeCV(uintCV(100000000)),
+        serializeCV(uintCV(parseUnits(sBtcAmount, 8)))
+    ]
+    const resp = await batchContractRead([call]);
 
-const aeUSDC = contractPrincipalCV("SP102V8P0F7JX67ARQ77WEA3D3CFB5XW39REDT0AM", "token-waeusdc");
-const stx = contractPrincipalCV("SP102V8P0F7JX67ARQ77WEA3D3CFB5XW39REDT0AM", "token-wstx-v2");
-const aBTC = contractPrincipalCV("SP2XD7417HGPRTREMKF748VNEQPDRR0RMANB7X1NK", "token-abtc");
-const sBTC = contractPrincipalCV("SP1E0XBN9T4B10E9QMR7XMFJPMA19D77WY3KP2QKC", "token-wsbtc");
-const aUSD = contractPrincipalCV("SP2XD7417HGPRTREMKF748VNEQPDRR0RMANB7X1NK", "token-susdt");
-const alex = contractPrincipalCV("SP102V8P0F7JX67ARQ77WEA3D3CFB5XW39REDT0AM", "token-alex");
-
-type SwapOption = { path: ClarityValue[], factors: ClarityValue[] }
-
-export const options: SwapOption[] = [
-    {
-        path: [sBTC, stx, aeUSDC],
-        factors: [factor1, factor1]
-    },
-    /* 
-    Runtime(MaxStackDepthReached, None) error occures on chain if we use long paths.
-    So commented out longer options for now.
-    {
-        path: [sBTC, alex, stx, aeUSDC],
-        factors: [factor1, factor1, factor1]
-    },
-    {
-        path: [sBTC, aBTC, aUSD, stx, aeUSDC],
-        factors: [factor05, factor1, factor1, factor1]
-    },
-    {
-        path: [sBTC, aBTC, stx, aeUSDC],
-        factors: [factor05, factor1, factor1]
+    if (resp[0] && resp[0].Ok) {
+        const js = cvToJSON(deserializeCV(resp[0].Ok));
+        if (js.success) {
+            return formatUnits(js.value.value, 8);
+        }
     }
-    */
-];
 
-export type SwapResult = {
-    option: SwapOption,
-    out: number
-}
-
-export const getBestSwap = async (sBtcAmount: number): Promise<SwapResult> => {
-    const dx = uintCV(parseUnits(sBtcAmount, 8));
-
-    const calls: ReadCall[] = options.map((option) => {
-        let fn = ''
-        if (option.path.length === 3) {
-            fn = 'get-helper-a'
-        } else if (option.path.length === 4) {
-            fn = 'get-helper-b'
-        } else if (option.path.length === 5) {
-            fn = 'get-helper-c'
-        }
-
-        if (!fn) {
-            throw new Error('Invalid path')
-        }
-
-        let functionArgs: ClarityValue[] = [
-            ...option.path,
-            ...option.factors,
-            dx
-        ];
-
-        return [
-            serializeCV(router),
-            fn,
-            ...functionArgs.map((v: any) => serializeCV(v))
-        ]
-    });
-
-    const resp = await batchContractRead(calls);
-
-    const results: SwapResult[] = resp.map((r, i) => {
-        if (r.Ok) {
-            const cv = deserializeCV(r.Ok);
-            if (cv.type === "err") {
-                return { option: options[i], out: 0 };
-            }
-
-            return { option: options[i], out: formatUnits(cvToJSON(cv).value.value, 8) };
-        }
-
-        return { option: options[i], out: 0 };
-    }).sort((a, b) => b.out - a.out);
-
-    return results[0];
+    return 0;
 }
