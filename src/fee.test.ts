9c402586a0c55c1ae6948038ebbd7fa366c88988
Talha Bugra Bulut <talhabugrabulut@gmail.com>
2025-08-15 09:17:31 +0300

    increase-tx-fees

--- src/fee.test.ts
+++ src/fee.test.ts
@@ -9,7 +9,7 @@ describe("estimateTxFeeOptimistic", () => {
                 getMempoolTransactions: () => ({ total: 1200 })
             }
         });
-        expect(await estimateTxFeeOptimistic()).toEqual(100000);
+        expect(await estimateTxFeeOptimistic()).toEqual(600000);
     });
 
     test("2", async () => {
@@ -18,7 +18,7 @@ describe("estimateTxFeeOptimistic", () => {
                 getMempoolTransactions: () => ({ total: 290 })
             }
         });
-        expect(await estimateTxFeeOptimistic()).toEqual(90000);
+        expect(await estimateTxFeeOptimistic()).toEqual(500000);
     });
 
     test("3", async () => {
@@ -27,7 +27,7 @@ describe("estimateTxFeeOptimistic", () => {
                 getMempoolTransactions: () => ({ total: 126 })
             }
         });
-        expect(await estimateTxFeeOptimistic()).toEqual(65000);
+        expect(await estimateTxFeeOptimistic()).toEqual(400000);
     });
 
     test("4", async () => {
@@ -36,7 +36,7 @@ describe("estimateTxFeeOptimistic", () => {
                 getMempoolTransactions: () => ({ total: 58 })
             }
         });
-        expect(await estimateTxFeeOptimistic()).toEqual(50000);
+        expect(await estimateTxFeeOptimistic()).toEqual(300000);
     });
 
     test("5", async () => {
@@ -45,6 +45,6 @@ describe("estimateTxFeeOptimistic", () => {
                 getMempoolTransactions: () => ({ total: 12 })
             }
         });
-        expect(await estimateTxFeeOptimistic()).toEqual(35000);
+        expect(await estimateTxFeeOptimistic()).toEqual(200000);
     });
 });
